7c2621a2bce04842a27c84e288578e143abf9ed6
eblanboi <tylerdelmas0@gmail.com>
2025-09-08 21:54:28 +0100

    Auth: Login/Index.tsx cleanup and moving isProcessing status to a separate page

--- src/pages/Auth/Login/Processing.tsx
+++ src/pages/Auth/Login/Processing.tsx
@@ -1,1 +1,26 @@
-export {}
+import { useTranslation } from "react-i18next"
+import { NavLink } from "react-router-dom"
+
+
+const LoginProcessingPage: React.FC = () => {
+    const { t } = useTranslation("auth");
+
+    return (
+        <>
+            <div className="hero__content">
+                <div>
+                    <h1>{t('processing.working')}</h1>
+                    <small>{t('processing.instructions')}</small>
+                </div>
+            </div>
+            <div className="">
+                <div className="loading-bar">
+                    <div className="loading-bar__progress"></div>
+                </div>
+                <small>{t('processing.tooLongPrompt')} <NavLink to={'/help'}>{t('processing.supportPrompt')}</NavLink></small>
+            </div>
+        </>
+    )
+}
+
+export default LoginProcessingPage

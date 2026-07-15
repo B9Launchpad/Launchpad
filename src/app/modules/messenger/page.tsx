d74b587896896e125e7ac901fe71394ba42adc87
eblanboi <98335821+JUST1PO1NT0@users.noreply.github.com>
2026-07-15 14:30:47 +0100

    feat: messenger components

---
+++ src/app/modules/messenger/page.tsx
@@ -0,0 +1,16 @@
+import MainLayout from "@/components/layout/MainLayout"
+import MessengerMessageList from "./MessageList";
+
+
+
+const Messenger: React.FC = () => {
+    return (
+        <MainLayout>
+            <MessengerMessageList></MessengerMessageList>
+            <h1>ААА! Пукич, какич, жидкая калость... 😏💅</h1>
+        </MainLayout>
+    )
+}
+
+
+export default Messenger;
--- src/components/layout/MainLayoutContent.tsx
+++ src/components/layout/MainLayoutContent.tsx
@@ -8,6 +8,7 @@ import SidebarComponent, { SidebarItems } from "./sidebar/Sidebar";
 import LayoutSettings from "./SettingsLayout";
 import { SettingsRegistryProvider } from "@/contexts/SettingsRegistryContext";
 import { PopupProvider } from "@/contexts/ModalContext";
+import { useUser } from "@/contexts/UserContext";
 
 interface MainLayoutProps {
     children: React.ReactNode;
@@ -15,10 +16,11 @@ interface MainLayoutProps {
 
 export default function MainLayoutContent({ children }: MainLayoutProps) {
     const { showSettings } = useView();
+    const user = useUser()
 
     const profile = {
-        name: ["Tatiana", "Yakovleva"],
-        email: "tyakovleva@b9creators.co.uk",
+        name: user.name,
+        email: user.email,
         picture: "/storage/d70ee478ead2fef85d9a86575b6d0315.webp"
     } satisfies ProfileProps;
 
